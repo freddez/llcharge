945d3e51abf19f06deef2c67fa25c35858a8e6e3
Frederic de Zorzi <f@idez.net>
2022-03-29 19:08:54 +0200

    +actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,66 @@
+use actix_web::{get, App, HttpServer};
+use once_cell::sync::Lazy;
 use std::process;
+use std::sync::Mutex;
 use std::{thread, time};
-
 mod cfg;
 mod input;
 mod sample;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cfg: cfg::MyConfig = confy::load("bat-plug")?;
-    let power_threshold = 0.6;
+static SAMPLE: Lazy<Mutex<sample::Sample>> = Lazy::new(|| Mutex::new(sample::Sample::default()));
 
-    let initial_pause = time::Duration::from_millis(2000);
-    let some_seconds = time::Duration::from_millis(1000);
-    let mut sample = sample::Sample::default();
-    ureq::get(&cfg.power_on_url).call()?;
-    println!("Power ON");
-    thread::sleep(initial_pause);
-    loop {
-        thread::sleep(some_seconds);
-        let message: input::Message = ureq::get(&cfg.status_url).call()?.into_json()?;
-        let power = message.get_power();
-        sample.insert(power);
-        if sample.is_ready() {
-            println!("{} avg : {}", power, sample.last_avg());
-            if power < power_threshold {
-                println!("Down to {} watts, Power OFF", { power_threshold });
-                // ureq::get(&cfg.power_off_url).call()?;
-                process::exit(1);
+#[get("/")]
+async fn index() -> String {
+    let last_avg = SAMPLE.lock().unwrap().last_avg();
+    format!("average of {last_avg}W")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    thread::spawn(|| {
+        let cfg: cfg::MyConfig = match confy::load("bat-plug") {
+            Ok(c) => c,
+            Err(error) => panic!("Problem opening the file: {:?}", error),
+        };
+
+        let power_threshold = 0.6;
+
+        let initial_pause = time::Duration::from_millis(2000);
+        let some_seconds = time::Duration::from_millis(1000);
+        match ureq::get(&cfg.power_on_url).call() {
+            Ok(a) => a,
+            Err(error) => panic!("Problem opening the file: {:?}", error),
+        };
+        println!("Power ON");
+        thread::sleep(initial_pause);
+        loop {
+            thread::sleep(some_seconds);
+            let message: input::Message = match match ureq::get(&cfg.status_url).call() {
+                Ok(a) => a,
+                Err(error) => panic!("Problem opening the file: {:?}", error),
             }
-        } else {
-            println!("{}", power);
+            .into_json()
+            {
+                Ok(a) => a,
+                Err(error) => panic!("Problem opening the file: {:?}", error),
+            };
+            let power = message.get_power();
+            SAMPLE.lock().unwrap().insert(power);
+            if SAMPLE.lock().unwrap().is_ready() {
+                println!("{} avg : {}", power, SAMPLE.lock().unwrap().last_avg());
+                if power < power_threshold {
+                    println!("Down to {} watts, Power OFF", { power_threshold });
+                    // ureq::get(&cfg.power_off_url).call()?;
+                    process::exit(1);
+                }
+            } else {
+                println!("{}", power);
+            }
         }
-    }
+    });
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
